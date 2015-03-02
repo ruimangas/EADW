61b2acbd80161502d290c13dc9462955a5106f4d
ruimangas <ruijosemangas@gmail.com>
2015-03-02 20:05:23 +0000

    txt

--- Labs/lab3_1.py
+++ Labs/lab3_1.py
@@ -1,14 +1,14 @@
-from collections import Counter
-
+import re
+dic={}
 def inverted_index(file=open("teste_file_ex1.txt","r")):
-	dic={}
+	line_spliter = re.compile('\W+')
 	my_count={}
-	var=0
+	var=1
 	for line in file.readlines():
-		for w in line.split():
+		for w in line_spliter.split(line):
 			try: my_count[w] += 1
 			except KeyError: my_count[w] = 1
-		for word in set(line.split()):
+		for word in set(line_spliter.split(line)):
 			dic.setdefault(word, []).append((var,my_count[word]))
 		var+=1
 		my_count.clear()
@@ -19,8 +19,7 @@ def print_dic(dictionary):
 		print key + " -> " + str(value)
 
 print_dic(inverted_index())
-
-
-
+print str(len(open("teste_file_ex1.txt","r").readlines())) + " documents"
+print str(len(dic.keys())) + " terms"
 
 
