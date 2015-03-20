1820054143cc19f051d1e94a502fea31a770483d
Rui Pereira <ruijosemangas@gmail.com>
2015-03-20 22:05:11 +0000

    push feito a pedido do pbraz

--- Labs/lab4/pageRank.py
+++ Labs/lab4/pageRank.py
@@ -1,20 +1,35 @@
+from __future__ import division
 
 # d -> dumping factor
 # l -> interactions
 # graph -> dictionary
 
+lines = open("aula04_links.txt").readline()
+inverted_index = {}
+
+for line in lines:
+	a = map(int, line.strip().split())
+	for link in a[1:]:
+		inverted_index.setdefault(link, set()).add(a[0])
+
 def pageRank(graph,d,l):
 	number = len(graph)
 	dic = {}
 	pageRank = {key: 1/len(graph) for key in graph.keys()}
-	for (key,value) in graph.items():
+	pageRank_next = pageRank.copy()
+	for (key,value) in graph.iteritems():
 		dic[key] = len(value)
-	for i in range(1,l):
+	for i in range(l):
 		for (j, val) in graph.iteritems():
-			pageRank[j] = d*1/number+(1-d)+sum([pageRank[link]/dic[link] for link in val])
+			pageRank_next[j] = d*(1/number)+(1-d)*sum([pageRank[link]/dic[link] for link in val])
+		pageRank = pageRank_next.copy()
 	return pageRank
 
-print pageRank({1:[2,3],2:[1,3],3:[1,2]},0.1,3)
+print pageRank({1:[3],2:[1,3],3:[1,2]},0.1,2)
+
+
+
+
 
 
 
