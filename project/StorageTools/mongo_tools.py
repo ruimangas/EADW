d7f1fc59fc88792b8a08df26bffb39a6823e12de
Rui Pereira <ruijosemangas@gmail.com>
2015-04-13 17:30:26 +0100

    now running with mongodb

---
+++ project/StorageTools/mongo_tools.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+
+def MongoConnection():
+	client = MongoClient('localhost', 27017)
+	return client
+
+def addNews(link,title,date,document):
+	client = MongoConnection()
+	db = client.test
+	news = db.news
+	newReport = {'link' : link, 'title' : title, 'date' : date, 'document' : document}
+	news.insert(newReport)
+
+def getNews(link):
+	client = MongoConnection()
+	db = client.test
+	news = db.news
+	report = news.find_one({"link" : link})
+
+	if report:
+		return report['title'] 
+	else: return "Empty"
+
+
+def getAllnews():
+	client = MongoConnection()
+	db = client.test
+	news = db.news
+
+	return news.find()
