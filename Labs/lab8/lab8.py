1348bc48f344d47b0653c0220c0375809b6246ea
Rui Pereira <ruijosemangas@gmail.com>
2015-05-06 10:20:06 +0100

    lab 8 updated

--- Labs/lab8/lab8.py
+++ Labs/lab8/lab8.py
@@ -16,12 +16,11 @@ def name_entities_ex2():
     file = open("aula03_cfc.txt", "r").readlines()
 
     for line in file:
-        sentences = nltk.sent_tokenize(line)
         tokens = nltk.word_tokenize(line)
         tagged = nltk.pos_tag(tokens)
         for k in nltk.chunk.ne_chunk(tagged, binary= True).subtrees():
             if k.label() == "NE":
-                tree_string = str(k).encode('utf-8')
+                tree_string = str(k)
                 print tree_string.split()[1].split("/")[0]
 
 name_entities_ex1()
