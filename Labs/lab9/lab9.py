7b5f0c4bad813a8d4c25e5c38d038e5b45770370
pebraz <pbraz.93@gmail.com>
2015-05-20 09:55:44 +0100

    finished lab9

--- Labs/lab9/lab9.py
+++ Labs/lab9/lab9.py
@@ -7,14 +7,26 @@ def remove_stop_words():
         if filen.startswith('stop'):
             with open('../lab6/' + filen) as f:
                 stop_words += map(lambda s:s.strip(), f.readlines())
-
     return stop_words
 
 def polarity(file = open('aula10_polarity.txt','r')):
-    return set(reduce(lambda a,b: a + b.split()[1:], file.readlines(), [])).difference(remove_stop_words())
+    if 'polarity.txt' in os.listdir('.'):
+        print "There is a cached word file, do you want to get a new one?[ y / n ]"
+        opt = raw_input(">> ")
+        if opt == 'n':
+            with open('polarity.txt') as f:
+                return map(lambda s:s.strip(),f.readlines())
+
+    num_words = int(raw_input("Select number of words to be chosen\n>>"))
+    words = reduce(lambda a,b: a+b.split()[1:], file.readlines(), [])
+    words = list(set(words).difference(remove_stop_words()))[:num_words] #return only 2000 words (have no clue if popular)        
+    with open('polarity.txt', 'w+') as f:
+        for word in words:
+            f.write(word + "\n")
+    return words
 
 FEATURES = polarity()
-
+print "polarity done."
 def get_features(input):
     words = input.split()
     features = {}
@@ -26,14 +38,14 @@ def get_features(input):
 def nbc_classifier(file = open('aula10_polarity.txt','r')):
     examples = []
     for line in file:
-        f_class = "positive" if line[0] == '+' else "negative"
-        examples.append((get_features(line),f_class))
+        examples.append((get_features(line),line[0]))
     
     return examples
 
+
 classifier = nltk.NaiveBayesClassifier.train(nbc_classifier())
 estimated_class = classifier.classify(get_features("Hot Tub Time Machine proved title when it earned fairly positive reviews and some decent money"))
-print estimated_class
+print "Class: [" + estimated_class + "]"
 
      
             
